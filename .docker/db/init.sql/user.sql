e8bef595aa3da0924f2d40b4317f9f07191a6b5e
cyrhades <cyrhades76@gmail.com>
2026-01-06 14:43:21 +0100

    retiré ambiguité sur utser 3

--- .docker/db/init.sql/user.sql
+++ .docker/db/init.sql/user.sql
@@ -9,7 +9,7 @@ CREATE TABLE IF NOT EXISTS `user` (
 
 insert into user (id, firstname, lastname, username, password) values (1, 'Admin', 'Immo', 'idor_admin_21', 'pistache_et_chocolat');
 insert into user (id, firstname, lastname, username, password) values (2, 'guest', 'guest', 'guest', 'guest');
-insert into user (id, firstname, lastname, username, password) values (3, 'Admin', '-', 'admin@boutik.com', 'c23fb44bf5c9d5ecd6030119b8f712fcb94c87ee');
+insert into user (id, firstname, lastname, username, password) values (3, 'Michel', 'michel', 'michel@rouge.com', 'c23fb44bf5c9d5ecd6030119b8f712fcb94c87ee');
 insert into user (id, firstname, lastname, username, password) values (4, 'Molly', 'Lourens', 'mlourens3@nih.gov', '548bb9db8603835aabe47476bfa844a50e070809');
 insert into user (id, firstname, lastname, username, password) values (5, 'Jeanine', 'Carpmile', 'jcarpmile4@github.io', '2e1ab01099aab2b78d477cf3469ff94f488aa9bd');
 insert into user (id, firstname, lastname, username, password) values (6, 'Ekaterina', 'Penhearow', 'epenhearow5@dropbox.com', 'ed4cdf5bee9f1fa28ef510f275b9797a8ec55060');
